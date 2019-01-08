e39d55eada982fb1ad99f9cd1672a052d9011618
minhoonkim <kmh357@tong.com>
2019-01-08 12:10:01 +0900

    Initial commit

---
+++ src/main/java/com/example/jpacoretest/JpaCoreTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.jpacoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCoreTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCoreTestApplication.class, args);
+    }
+
+}
+
